6909bb461ff253da764692652a8b078afa94849c
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-01-04 08:17:42 -0800

    Active Record exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+@store1.employees.create(first_name: 'Jimmy', last_name: 'Conway', hourly_rate: 300)
+@store1.employees.create(first_name: 'Franky', last_name: 'Carbone', hourly_rate: 300)
+@store2.employees.create(first_name: 'Jimmy', last_name: 'Two Times', hourly_rate: 300)
+@store2.employees.create(first_name: 'Johnny', last_name: 'Roastbeef', hourly_rate: 300)
+@store2.employees.create(first_name: 'Tommy', last_name: 'Devito', hourly_rate: 300)
+
